8c8b5d80d20a9c08854a072d329c508ff1f48f49
Robbie Cao <robbie.cao@gmail.com>
2016-12-28 08:31:22 +0800

    Config serial port, get available bytes with ioctl and misc
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- src/iod.c
+++ src/iod.c
@@ -10,10 +10,11 @@
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/select.h>
+#include <sys/ioctl.h>
 
 #include "log.h"
 
-#define TTY_UART    "/dev/ttyS2"
+#define TTY_UART            "/dev/ttyS2"
 
 #define BUFFER_SIZE         256
 
@@ -22,16 +23,9 @@ static uint8_t buf_in[BUFFER_SIZE];
 static uint8_t buf2[BUFFER_SIZE / 2];
 #endif
 
-int main(void)
+void sanity(void)
 {
-    const char *LOG_TAG = "IOD";
     int            fd;
-    struct termios options, old;
-    fd_set         readfds;
-
-    int idx = 0;
-
-    fprintf(stdout, "Welcome - %s %s\n", __DATE__, __TIME__);
 
     /* Open the port */
     fd = open(TTY_UART, O_RDWR | O_NOCTTY | O_NDELAY);
@@ -41,20 +35,19 @@ int main(void)
     }
     fcntl(fd, F_SETFL, 0);
 
-#if 0
     // Test write
-    while (1) {
+    if (1) {
         int n = write(fd, "ATZ\r\n", 5);
         if (n < 0) {
-            fputs("write() of 4 bytes failed!\n", stderr);
+            fputs("write() of 5 bytes failed!\n", stderr);
         } else {
-            fputs("write() of 4 bytes success!\n", stderr);
+            fputs("write() of 5 bytes success!\n", stderr);
         }
         sleep(1);
     }
 
     // Test read
-    while (1) {     /* loop until we have a terminating condition */
+    if (1) {     /* loop until we have a terminating condition */
         /* read blocks program execution until a line terminating character is
          * input, even if more than 255 chars are input. If the number
          * of characters read is smaller than the number of chars available,
@@ -67,70 +60,81 @@ int main(void)
         buf[res] = 0;             /* set end of string, so we can printf */
         printf(":%s:%d\n", buf, res);
     }
-#endif
 
-#if 0
+    close(fd);
+}
+
+int main(void)
+{
+    const char *LOG_TAG = "IOD";
+    int            fd;
+    struct termios options, old;
+    fd_set         readfds;
+
+    fprintf(stdout, "Welcome - %s %s\n", __DATE__, __TIME__);
+
     /*
-     * Get the current options for the port...
+     * Init
      */
+    // Open the port
+    fd = open(TTY_UART, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd < 0) {
+        perror(TTY_UART);
+        exit(-1);
+    }
+    fcntl(fd, F_SETFL, 0);
+
+    // Get the current options for the port...
     tcgetattr(fd, &old);
+    options = old;
 
-    bzero(&options, sizeof(options));
+    // BAUDRATE: Set bps rate. You could also use cfsetispeed and cfsetospeed.
+    // CS8     : 8n1 (8bit, no parity, 1 stopbit)
+    // CLOCAL  : local connection, no modem contol
+    // CREAD   : enable receiving characters
+    options.c_cflag = B115200 | CS8 | CLOCAL | CREAD;
+    options.c_iflag = IGNPAR;
+    options.c_oflag = 0;
+    options.c_lflag = 0;
 
-    /*
-     * BAUDRATE: Set bps rate. You could also use cfsetispeed and cfsetospeed.
-     * CRTSCTS : output hardware flow control (only used if the cable has
-     * all necessary lines. See sect. 7 of Serial-HOWTO)
-     * CS8     : 8n1 (8bit,no parity,1 stopbit)
-     * CLOCAL  : local connection, no modem contol
-     * CREAD   : enable receiving characters
-     */
-    options.c_cflag = B115200 /* | CRTSCTS */ | CS8 | CLOCAL | CREAD;
+    options.c_cc[VMIN]  = 0;        // block until n bytes are received
+    options.c_cc[VTIME] = 0;        // block until a timer expires (n * 100 ms)
 
-    /*
-     * Clean the modem line
-     */
+    // Clean the modem line
     tcflush(fd, TCIFLUSH);
 
-    /*
-     * Set the new options for the port...
-     */
-    tcsetattr(fd, TCSANOW, &options);
-#endif
-    tcgetattr(fd, &options);
-    options.c_cc[VMIN]  = 0;
-    options.c_cc[VTIME] = 10;
+    // Set the new options for the port...
     tcsetattr(fd, TCSANOW, &options);
 
+
     FD_ZERO(&readfds);
-    FD_SET(STDIN_FILENO, &readfds); /* stdin */
     FD_SET(fd, &readfds);           /* ttyS2 */
 
     while (1) {
-        // Wait for input from stdin
-        if (select(fd + 1, &readfds, NULL, NULL, NULL)) {
-            int c;
+        int res;
 
-            printf("Wakeup\n");
-            idx = 0;
+        // Wait for input from ttyS2
+        res = select(fd + 1, &readfds, NULL, NULL, NULL);
 
-            if (FD_ISSET(STDIN_FILENO, &readfds)) {
-                printf("fd 0 is set\n");
-                // Store input into buf_in, newline ('\n') as input done
-                while ((c = fgetc(stdin)) != '\n') {
-                    buf_in[idx++] = c;
-                }
-                buf_in[idx] = '\0';
-            }
-            if (FD_ISSET(fd, &readfds)) {
-                printf("fd %d is set\n", fd);
-                // Store input into buf_in, newline ('\n') as input done
-                int res = read(fd, buf_in, 255);
-                buf_in[res] = '\0';
-            }
-
-            LOGD(LOG_TAG, "%s\n", buf_in);
+        if ((res < 0) && (EINTR == errno)) {
+            // The system call was interupted by a signal and a signal handler was
+            // run.  Restart the interupted system call.
+            fprintf(stderr, "errro on select!\n");
         }
+
+        if (FD_ISSET(fd, &readfds)) {
+            int nr;
+            int available;
+
+            // Retrieve the count of the incoming bytes
+            res = ioctl(fd, FIONREAD, &available);
+
+            // Store input into buf_in, newline ('\n') as input done
+            nr = read(fd, buf_in, available);
+            buf_in[nr] = '\0';
+        }
+
+        LOGD(LOG_TAG, "%s\n", buf_in);
 
         if (strcmp((char *)buf_in, "quit") == 0) {
             break;
@@ -156,6 +160,9 @@ int main(void)
         cmd_parse(buf2, len, cmd_callback);
 #endif
     }
+
+    // Restore port config
+    tcsetattr(fd, TCSANOW, &old);
 
     close(fd);
 
